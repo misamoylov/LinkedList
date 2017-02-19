3e2a924533d08ce808eda242f2e834c01aa76d1e
Mikhail Samoylov <mikhailsamoiloff@gmail.com>
2017-02-19 20:06:27 +0300

    3

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -1,13 +1,6 @@
-# Python program to reverse a linked list
-# Time Complexity : O(n)
-# Space Complexity : O(1)
-
-# Node class
-
 
 class Node:
 
-    # Constructor to initialize the node object
     def __init__(self, val):
         self.val = val
         self.next = None
@@ -16,11 +9,9 @@ class Node:
 
 class LinkedList:
 
-    # Function to initialize head
     def __init__(self):
         self.head = None
 
-    # Function to reverse the linked list
     def reverse(self):
         prev = None
         current = self.head
@@ -31,13 +22,11 @@ class LinkedList:
             current = next
         self.head = prev
 
-    # Function to insert a new node at the beginning
-    def push(self, new_val):
+    def add(self, new_val):
         new_node = Node(new_val)
         new_node.next = self.head
         self.head = new_node
 
-    # Utility function to print the linked LinkedList
     def printList(self):
         temp = self.head
         while(temp):
@@ -47,7 +36,6 @@ class LinkedList:
     # Time:  O(n)
     # Space: O(1)
     def copyRandomList(self, head):
-        # copy and combine copied list with original list
         current = head
         while current:
             copied = Node(current.val)
@@ -55,14 +43,12 @@ class LinkedList:
             current.next = copied
             current = copied.next
 
-        # update random node in copied list
         current = head
         while current:
             if current.random:
                 current.next.random = current.random.next
             current = current.next.next
 
-        # split copied list from combined one
         dummy = Node(0)
         copied_current, current = dummy, head
         while current:
@@ -74,8 +60,6 @@ class LinkedList:
     # Time:  O(n)
     # Space: O(n)
     def copyRandomListAlt(self, head):
-        # @param head, a RandomListNode
-        # @return a RandomListNode
         dummy = Node(0)
         current, prev, copies = head, dummy, {}
 
@@ -94,12 +78,11 @@ class LinkedList:
         return dummy.next
 
 
-# Driver program to test above functions
 llist = LinkedList()
-llist.push(20)
-llist.push(4)
-llist.push(15)
-llist.push(85)
+llist.add(20)
+llist.add(4)
+llist.add(15)
+llist.add(85)
 
 print "Given Linked List"
 llist.printList()
